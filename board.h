eb805965fcdeffede0f7f48d8dbdf3e746afd9b8
1102-English-Samantha <thesamenglish@gmail.com>
2026-04-29 18:47:51 +0000

    changes

---
+++ board.h
@@ -0,0 +1,10 @@
+#ifndef BOARD_H
+#define BOARD_H
+using namespace std;
+
+class Board{
+
+
+};
+
+#endif
--- player.h
+++ player.h
@@ -1,7 +1,6 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "board.h"
-using namespace std;
 
 
 class Player{
