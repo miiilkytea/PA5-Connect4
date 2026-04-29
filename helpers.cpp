6e4fc3d365305236b49043bfed6c14790c534949
isabelle schinzing <izmiiilkytea@gmail.com>
2026-04-29 19:42:55 +0000

    helpers file updates

---
+++ helpers.cpp
@@ -0,0 +1,20 @@
+#include "helpers.h"
+
+int displayMenu() {
+    int selection;
+
+    cout << "*----------------MENU----------------*";
+    cout << "1. START NEW GAME" << endl;
+    cout << "2. SHOW PLAYER STATISTICS" <<endl;
+    cout << "3. EXIT" << endl;
+    cout << "Please select a menu option: ";
+
+    cin << selection;
+    if (validateInput)
+}
+
+template <typename T>
+bool validateInput(T in1, T in2, T uInput) {
+    if (uInput > in1 && uInput)
+}
+// start, statistics, end
---
+++ helpers.h
@@ -0,0 +1,7 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+int displayMenu();
+
+template <typename T>
+bool validateInput(T, T, T);
