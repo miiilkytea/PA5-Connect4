5aa947c279b67ae7cd0ea8222a8e432eb0a54ad0
1102-English-Samantha <thesamenglish@gmail.com>
2026-04-29 18:59:27 +0000

    Board class creation

--- board.h
+++ board.h
@@ -3,8 +3,30 @@
 using namespace std;
 
 class Board{
+    int rows, cols;
+    char** grid;
 
+    public:
+        Board();
+        Board(int, int);
+        Board(const Board&);
 
+        int getRows();
+        int getCols();
+        char** getGrid();
+
+        void setRows(int);
+        void setCols(int);
+
+        Board& operator =(const Board&);
+        ~Board();
+
+        bool dropPiece(int, char);
+        bool isValidMove(int);
+        bool checkWin(char);
+        bool checkTie();
+
+        ostream& operator <<(ostream&, Board&);
 };
 
 #endif
