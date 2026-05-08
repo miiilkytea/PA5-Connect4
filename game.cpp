6e2451cb28763f782b104d785a6659f7a4bbe882
isabelle schinzing <izmiiilkytea@gmail.com>
2026-05-08 22:05:43 +0000

    game updates

--- game.cpp
+++ game.cpp
@@ -1,40 +1,58 @@
 #include "game.h"
 
 Game::Game() {
-
+    player1 = null;
+    player2 = null;
+    currentPlayer = player1;
 }
 
 Game(Player* p1, Player* p2) {
     player1 = p1;
     player2 = p2;
+    currentPlayer = p1;
 }
 Game::Game(const Game& rh) {
     player1 = rh.p1;
     player2 = rh.p2;
+    
 }
 
 Player* Game::getP1() {
-
+    return player1;
 }
 Player* Game::getP2() {
-
+    return player2;
 }
 Player** Game::getCurrentPlayer(){
-
+    return currentPlayer;
 }
 Board* Game::getBoard() {
-
+    return board;
 }
 
 void Game::start() {
 
 }
 void Game::switchPlayer() {
-
+    if(currentPlayer == player1) {
+        currentPlayer = player2;
+    } else {
+        currentPlayer = player1;
+    }
 }
 void Game::playTurn() {
-
-}
+    bool check;
+    if (currentPlayer != null) {
+        do {
+            int selectedCol = currentPlayer->makeMove();
+            char piece = currentPlayer->getPiece();
+            check = board.dropPiece(selectedCol, piece);
+            if (board)
+            switchPlayer();
+        while (check == false);
+        }
+    } 
+}  
 void Game::savePlayerData() {
 
 }
